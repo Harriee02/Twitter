64528e921e219297986a7c23454ddfb777dbe006
Harrie Oriowo <harriee@fb.com>
2022-06-08 13:06:01 -0400

    Commit to implement majority of the .xml files

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,5 +1,6 @@
 package com.codepath.apps.restclienttemplate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -7,8 +8,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -23,17 +27,6 @@ import okhttp3.Headers;
 
 public class TimelineActivity extends AppCompatActivity {
 
-//    public void onLogoutButton(View view) {
-//        // forget who's logged in
-//        TwitterApp.getRestClient(this).clearAccessToken();
-//
-//        // navigate backwards to Login screen
-//        Intent i = new Intent(this, LoginActivity.class);
-////        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // this makes sure the Back button won't work
-////        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // same as above
-//        startActivity(i);
-//        finish();
-//    }
     public static final String TAG="TimelineActivity";
     TwitterClient client;
     RecyclerView rvTweets;
@@ -65,6 +58,22 @@ public class TimelineActivity extends AppCompatActivity {
         rvTweets.setLayoutManager(new LinearLayoutManager(this));
         rvTweets.setAdapter(adapter);
         populateHomeTimeline();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.compose){
+            Intent i = new Intent(TimelineActivity.this, ComposeActivity.class);
+            startActivity(i);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void populateHomeTimeline() {
