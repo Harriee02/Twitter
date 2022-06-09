ccacd028b43be1c2dc26c10955ca8b78236b2b25
Harrie Oriowo <harriee@fb.com>
2022-06-09 19:56:13 -0400

    This commits adds images to tweets, adds users tag name and also the timestamp

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,9 +1,11 @@
 package com.codepath.apps.restclienttemplate;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -19,38 +21,34 @@ import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.parceler.Parcel;
+import org.parceler.Parcels;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 import okhttp3.Headers;
 
 public class TimelineActivity extends AppCompatActivity {
+    private EndlessRecyclerViewScrollListener scrollListener;
 
     public static final String TAG="TimelineActivity";
+    private final int REQUEST_CODE = 20;
+    private SwipeRefreshLayout swipeContainer;
     TwitterClient client;
     RecyclerView rvTweets;
     List<Tweet> tweets;
     TweetAdapter adapter;
-    Button logOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
-        logOut = findViewById(R.id.logOut);
-        logOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TwitterApp.getRestClient(TimelineActivity.this).clearAccessToken();
-
-                // navigate backwards to Login screen
-                Intent i = new Intent(TimelineActivity.this, LoginActivity.class);
-//        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // this makes sure the Back button won't work
-//        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // same as above
-                startActivity(i);
-                finish();
-            }
-        });
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+//        rvItems.addOnScrollListener(scrollListener);
         client = TwitterApp.getRestClient(this);
         rvTweets = findViewById(R.id.rvTweets);
         tweets = new ArrayList<>();
@@ -58,7 +56,64 @@ public class TimelineActivity extends AppCompatActivity {
         rvTweets.setLayoutManager(new LinearLayoutManager(this));
         rvTweets.setAdapter(adapter);
         populateHomeTimeline();
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                fetchTimelineAsync(0);
+            }});
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+
     }
+//
+public void fetchTimelineAsync(int page) {
+    // Send the network request to fetch the updated data
+    // `client` here is an instance of Android Async HTTP
+    // getHomeTimeline is an example endpoint.
+    populateHomeTimeline();
+    adapter.clear();
+    adapter.addAll(tweets);
+    swipeContainer.setRefreshing(false);
+//    adapter.clear();
+//    client.getHomeTimeline(new JsonHttpResponseHandler() {
+//        @Override
+//        public void onSuccess(int statusCode, Headers headers, JSON json) {
+//            Log.i("WHEE","this works!");
+//            populateHomeTimeline();
+//            swipeContainer.setRefreshing(false);
+//        }
+//
+//        @Override
+//        public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+//            Log.d("DEBUG", "Fetch timeline error: " + throwable.toString());
+//        }
+//    });
+
+
+
+
+//        RecyclerView rvItems = (RecyclerView) findViewById(R.id.rvTweets);
+//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+//        rvItems.setLayoutManager(linearLayoutManager);
+//        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+//            @Override
+//            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+//                // Triggered only when new data needs to be appended to the list
+//                // Add whatever code is needed to append new items to the bottom of the list
+//                loadNextDataFromApi(page);
+//            }
+//        };
+
+    }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -70,10 +125,34 @@ public class TimelineActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.compose){
             Intent i = new Intent(TimelineActivity.this, ComposeActivity.class);
-            startActivity(i);
+            startActivityForResult(i,REQUEST_CODE);
             return true;
         }
+        if (item.getItemId() == R.id.logOut){
+            logOut();
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK){
+            Tweet tweet = Parcels.unwrap(data.getParcelableExtra("tweet"));
+            tweets.add(0, tweet);
+            adapter.notifyItemInserted(0);
+            rvTweets.smoothScrollToPosition(0);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void logOut() {
+        TwitterApp.getRestClient(this).clearAccessToken();
+
+        // navigate backwards to Login screen
+        Intent logOut = new Intent(this, LoginActivity.class);
+        logOut.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // this makes sure the Back button won't work
+        logOut.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // same as above
+        startActivity(logOut);
     }
 
     private void populateHomeTimeline() {
@@ -98,4 +177,7 @@ public class TimelineActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
 }
--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
@@ -1,6 +1,8 @@
 package com.codepath.apps.restclienttemplate;
 
 import android.content.Context;
+import android.content.Intent;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,13 +13,27 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
 public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder>{
     Context context;
     List<Tweet> tweets;
+
+    public void clear() {
+        tweets.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items -- change to type used
+    public void addAll(List<Tweet> list) {
+        tweets.addAll(list);
+        notifyDataSetChanged();
+    }
 
     public TweetAdapter(Context context, List<Tweet> tweets){
         this.context = context;
@@ -46,18 +62,44 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder>{
         ImageView ivProfileImage;
         TextView tvScreenName;
         TextView tvBody;
+        ImageView tweetImage;
+        TextView tweetTime;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
             tvBody = itemView.findViewById(R.id.tvBody);
             tvScreenName = itemView.findViewById(R.id.tvScreenName);
+            tweetImage = itemView.findViewById(R.id.tweetImage);
+            tweetTime = itemView.findViewById(R.id.tweetTime);
+
         }
         public void bind(Tweet tweet){
             tvBody.setText(tweet.body);
-            tvScreenName.setText(tweet.user.screenName);
-            Glide.with(context).load(tweet.user.profileImageUrl).into(ivProfileImage);
+            tvScreenName.setText(tweet.user.screenName);// Experimenting with   changing the screenName to the created time but the text is not changing.
+            //Where does
+            Glide.with(context)
+                    .load(tweet.user.profileImageUrl)
+                    .transform(new RoundedCorners(75))
+                    .into(ivProfileImage);
+            if (!tweet.image.equals("")){
+                tweetImage.setVisibility(View.VISIBLE);
+                Glide.with(context)
+                        .load(tweet.image)
+                        .centerCrop()
+                        .transform(new RoundedCorners(75))
+                        .into(tweetImage);
+            }
+            else{
+                tweetImage.setVisibility(View.GONE);
+            }
+            tweetTime.setText(tweet.getRelativeTimeAgo(tweet.createdAt));
+
+
         }
     }
+
+
+
 
 }
